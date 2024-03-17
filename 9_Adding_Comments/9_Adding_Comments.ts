5703646fd2902aaedde8cbe27b1f7aca20c5e8ba
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-17 12:17:49 -0700

    9_Adding_Comments
    9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ 9_Adding_Comments/9_Adding_Comments.ts
@@ -0,0 +1,8 @@
+
+
+// Comments added by: Bilal Ahmed
+// Date: Saturday Feb 17
+
+// This program will print: Hello World
+
+console.log('Hello World')
---
+++ 9_Adding_Comments/9_Adding_Comments2.ts
@@ -0,0 +1,6 @@
+// Comments added by: Bilal Ahmed
+// Date: Saturday Feb 17
+
+// This program will add two digits.
+
+console.log(5+5);
