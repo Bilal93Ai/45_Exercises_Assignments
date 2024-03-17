e9d150c328c98d1ae72fbccd2b084984f0a53028
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-17 13:56:18 -0700

    11_Greeting
    11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.

---
+++ 11_Greeting/11_Greeting.ts
@@ -0,0 +1,9 @@
+
+let names :string[] = ['Shoukat','Lalwa','Lalo', 'Jagga'] ;
+
+const message :string = 'Welcome to Governor Initiative Program: '
+
+for(let i=0; i<names.length; i++){
+    
+    console.log(message + names[i]) ;
+}
