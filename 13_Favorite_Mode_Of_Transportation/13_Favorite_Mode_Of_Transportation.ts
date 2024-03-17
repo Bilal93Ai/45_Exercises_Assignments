c12bd0601de89984d01f01b28cb9cdfb98e92fe0
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-17 14:07:09 -0700

    13_Favorite_Mode_Of_Transportation
    13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
    to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ 13_Favorite_Mode_Of_Transportation/13_Favorite_Mode_Of_Transportation.ts
@@ -0,0 +1,7 @@
+
+let Transportation :string[] = ['MotorBike','Car','Train','Aeroplane'] ;
+
+for(let i=0; i<Transportation.length; i++){
+    
+    console.log('I would like to own a ' + Transportation[i])
+}
