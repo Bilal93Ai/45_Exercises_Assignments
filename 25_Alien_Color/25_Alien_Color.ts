bf8411d4650dcaea26fb137aa3dd9bea63e87bb8
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:45:19 -0700

    Alien_Colors
    25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
    variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    
    • Write an if statement to test whether the alien’s color is green. If it is, print
    a message that the player just earned 5 points.
    
    • Write one version of this program that passes the if test and another that
    fails. (The version that fails will have no output.)

---
+++ 25_Alien_Color/25_Alien_Color.ts
@@ -0,0 +1,11 @@
+// creating an alien_color variable
+
+let alien_color :string = 'green';
+if (alien_color === 'green'){
+    console.log('you have earned 5 points !');
+}
+
+alien_color = 'yellow';
+if (alien_color === 'green'){
+    console.log();
+}
