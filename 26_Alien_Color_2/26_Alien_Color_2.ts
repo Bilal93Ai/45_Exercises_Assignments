77e4ab554d59a93eeadc8b32043c791e78e7db6c
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:46:26 -0700

    Alien_Color_2
    write an if-else chain.
    
    • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
    
    • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
    
    • Write one version of this program that runs the if block and another that runs the else block.

---
+++ 26_Alien_Color_2/26_Alien_Color_2.ts
@@ -0,0 +1,17 @@
+
+
+let alienColor :string = "green";
+if (alienColor === "green"){
+    console.log("you have earned 5 points!");
+}
+else{
+    console.log("you have earned 10 points");
+}
+
+alienColor = "yellow";
+if (alienColor === "green"){
+    console.log("you have earned 5 points!");
+}
+else{
+    console.log("You have earned 10 points!")
+}
