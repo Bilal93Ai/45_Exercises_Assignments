4f99f6fca5119f3375c5725eb337287b322c3e15
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:43:00 -0700

    International_Error
    22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
    to produce an index error. Make sure you correct the error before closing the program.

---
+++ 22_International_Error/22_International_Error.ts
@@ -0,0 +1,9 @@
+// if you haven't received an array index error in one of your program yet, try to make one happen.
+
+let transportation :string[] = ['car','bike','plane'];
+
+console.log(transportation[3]);
+
+// make sure you correct the error before closing the program
+
+console.log(transportation)
