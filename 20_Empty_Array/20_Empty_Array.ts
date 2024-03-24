cdd60f56416fe4c820cf0f35d31f31794b43ebe2
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:41:33 -0700

    Empty Array
    20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
    else you’d like. Write a program that creates a list containing these items.

---
+++ 20_Empty_Array/20_Empty_Array.ts
@@ -0,0 +1,13 @@
+
+// Think of array you could store in an array
+let items :string[] = ['Japan','Herosima','Nagasaki','Seoul'];
+
+// New method to store something in an empty array:
+// Write a program that creates a list containing these items.
+let items2 :string[] = [];
+// .push will read from 0 index
+items2.push('Karachi');
+items2.push('Pakistan');
+items2.push('China');
+
+console.log(items2);
