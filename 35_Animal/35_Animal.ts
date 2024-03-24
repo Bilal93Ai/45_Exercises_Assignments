c3b6bd1202e1bec091165f7ae2678540c819c913
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:53:08 -0700

    Animal
    35. Animals: Think of at least three different animals that have a common characteristic.
    Store the names of these animals in a list, and then use a for loop to
    print out the name of each animal.
    • Modify your program to print a statement about each animal, such as
    A dog would make a great pet.
    • Add a line at the end of your program stating what these animals have in
    common. You could print a sentence such as Any of these animals would
    make a great pet!

---
+++ 35_Animal/35_Animal.ts
@@ -0,0 +1,31 @@
+// 35. Animals: Think of at least three different animals that have a common characteristic.
+// Store the names of these animals in a list, and then use a for loop to
+// print out the name of each animal.
+// • Modify your program to print a statement about each animal, such as
+// A dog would make a great pet.
+// • Add a line at the end of your program stating what these animals have in
+// common. You could print a sentence such as Any of these animals would
+// make a great pet!
+
+
+const animal:string[] = ['Cat','Dog','Lion'];
+
+// animal.forEach(pets => {
+//     if(pets === 'Cat'){
+//         console.log(`A ${pets} is a great pet`);
+//     }
+//     else if (pets === 'Dog'){
+//         console.log(`A ${pets} would make a great pet.`);
+//     }
+//     else if (pets === 'Lion'){
+//         console.log(`A ${pets} is a dangerous pet.`);
+//     }
+// });
+
+// console.log('Any of these animals would make a great pet!');
+
+
+
+animal.forEach(petT => {
+
+})
