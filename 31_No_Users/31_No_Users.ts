e3b539f336a2ecb3f2bcc9c6db07dfb06b7b79dd
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:50:23 -0700

    No_Users
    31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
    
    • If the list is empty, print the message We need to find some users!
    
    • Remove all of the usernames from your array, and make sure the correct message is printed.

---
+++ 31_No_Users/31_No_Users.ts
@@ -0,0 +1,12 @@
+// from ex 28 make sure list of user names is not empty...1 if list is empty print we need to find some users 
+// 2 remove all usersnames frim arr and make sure correct message is been typed 
+
+const userNames: string[] = ['Admin','BK ModTest','Zulu Kobra','lePepsi','Dumais'];
+
+if(userNames.length === 0){
+    console.log('we need to find some users');
+}
+else{
+    let userNames = [];
+    console.log('All usernames have been removed. ' + userNames.length);
+}
