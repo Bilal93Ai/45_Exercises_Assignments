44afc11dd4a1603209dad75e7bc234ef799d5ba5
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-23 22:43:53 -0700

    Conditional_Tests
    23. Conditional Tests: Write a series of conditional tests. Print a statement
    describing each test and your prediction for the results of each test. Your code
    should look something like this:
    
    let car = 'subaru';
    
    console.log("Is car == 'subaru'? I predict True.")
    
    console.log(car == 'subaru')
    
    • Look closely at your results, and make sure you understand why each line evaluates to True or False.
    
    • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.

---
+++ 23_Conditional_Test/23_Conditional_Test.ts
@@ -0,0 +1,58 @@
+
+// = kisi value ko set kar rahe ha
+// == kisi value ko check karna 
+// === kisi value ko uske data type k saath check karna
+
+
+// write a series of conditional tests
+
+let car :string = 'subaru';
+
+// test no.1
+console.log("Is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');
+
+// test no.2
+console.log("Is car === 'subaru'? I predict true.");
+console.log(car === 'subaru');
+
+// != not equal
+// 2 != 2 false
+
+// test no.3
+console.log("Is car != 'Honda'? I predict true.");
+console.log(car != 'Honda');
+
+
+// test no.4
+console.log("Is car !== 'Ford'? I predict true.");
+console.log(car !== 'Ford');
+
+
+// test no.5
+console.log("Is car.upper case == 'SUBARU'? I predict true.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+
+// test no.6
+console.log("Is car == 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+
+
+// test no.7
+console.log("Is car === 'SUBARU'? I predict false.");
+console.log(car === 'SUBARU');
+
+// test no.8
+console.log("Is car === 'Toyota'? I predict false.");
+console.log(car === 'Toyota');
+
+// test no.9
+console.log("Is car === 'Mercedez'? I predict false.");
+console.log(car === 'Mercedez');
+
+// test no.10
+console.log("Is car === 'Indigo'? I predict false.");
+console.log(car === 'Indigo');
+
+
