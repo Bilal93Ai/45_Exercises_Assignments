6c4e6df096a04533fd4423fefc66ddb8908ef622
Bilal93Ai <160306149+Bilal93Ai@users.noreply.github.com>
2024-03-24 20:10:23 -0700

    36_TShirt
    36. T-Shirt: Write a function called make_shirt() that accepts a size and the
    text of a message that should be printed on the shirt. The function should print
    a sentence summarizing the size of the shirt and the message printed on it.
    Call the function.

---
+++ 36_TShirt/36_T_Shirt.ts
@@ -0,0 +1,23 @@
+
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. The function should print
+// a sentence summarizing the size of the shirt and the message printed on it.
+
+// Call the function.
+
+function make_shirt(size:any,text:any){
+    console.log(`Creating a ${size} shirt size with a logo: ${text}`);
+}
+make_shirt('Small',`Nike`);
+make_shirt('Medium',`Nike`);
+make_shirt('Large',`Nike`);
+
+
+
+// Practice
+
+
+function make_shoes(size:any,text:any){
+    console.log(`Making 100 ${size} shoes with a logo: ${text}`);
+}
+make_shoes('Small','Nike');
